00b094ae033a1de6212b2f37525d0b47cf648c64
GBTC440019UR <GBTC440019UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-02-14 14:43:05 -0500

    added backgrounds

--- src/main/java/com/austin/blog/HomeController.java
+++ src/main/java/com/austin/blog/HomeController.java
@@ -26,4 +26,35 @@ public class HomeController {
         return "contact";
     }
 
+    @RequestMapping("/blog/sbvsb")
+    public String sbvsb() {
+        return "sbvsb";
+    }
+
+    @RequestMapping("/blog/inheritance")
+    public String inheritance() {
+        return "inheritance";
+    }
+
+    @RequestMapping("/blog/ghwt")
+    public String ghwt() {
+        return "ghwt";
+    }
+
+    @RequestMapping("/blog/abstraction")
+    public String abstraction() {
+        return "abstraction";
+    }
+
+    @RequestMapping("/blog/loops")
+    public String loops() {
+        return "loops";
+    }
+
+    @RequestMapping("/blog/ght")
+    public String ght() {
+        return "ght";
+    }
 }
+
+
